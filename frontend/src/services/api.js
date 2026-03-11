f19bc6b8e06865aeec578c4d34dd49d31fd45c49
Luke Compton-Burnett <lcompton-burnett@hotmail.com>
2026-03-11 21:25:14 +0000

    edited frontend api url

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,6 +1,6 @@
 import { getToken } from "../stores/auth";
 
-const BASE_URL = import.meta.env.VITE_API_BASE_URL || "http://127.0.0.1:8000/api/v1";
+const BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 function prettyFieldName(rawField) {
   return rawField
